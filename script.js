d176e4c61f43be73112cb4c8294011c81bd91de2
JingYee-JY <jingyeetan2005@gmail.com>
2022-08-26 14:31:26 +0800

    i

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-let startGame = true
+const startButton = document.querySelector(".startButton")
+const start = document.querySelector(".start")
+const game = document.querySelector(".game")
+const playAgain = document.querySelector(".againButton")
+const final = document.querySelector(".final");
+const board = document.getElementById("board");
+const pieces = document.getElementById("pieces");
+
+let startGame = false
 
 var rows = 2
 var columns = 3;
@@ -6,33 +14,89 @@ var columns = 3;
 var currTile;
 var otherTile;
 
-RenewingTraditionG = [
-    {number: 1, image: "./img/Renewing TraditionG11.png"},
-    {number: 2, image: "./img/Renewing TraditionG12.png"},
-    {number: 3, image: "./img/Renewing TraditionG13.png"},
-    {number: 4, image: "./img/Renewing TraditionG21.png"},
-    {number: 5, image: "./img/Renewing TraditionG22.png"},
-    {number: 6, image: "./img/Renewing TraditionG23.png"}
+var startingX, startingY, movingX, movingY;
+
+  function touchstart(evt) {
+    startingX = evt.touches[0].clientX
+    startingY = evt.touches[0].clientY
+    dragStart()
+  }
+  function touchmove(evt) {
+    movingX = evt.touches[0].clientX
+    movingY = evt.touches[0].clientY
+    dragOver(evt)
+  } 
+  function touchend() {
+    dragDrop()
+    dragEnd()
+}
+
+MixingManyLanguagesP = [
+    {number: 1, image: "./img/Mixing-Many-Languages_01.jpg"},
+    {number: 2, image: "./img/Mixing-Many-Languages_02.jpg"},
+    {number: 3, image: "./img/Mixing-Many-Languages_03.jpg"},
+    {number: 4, image: "./img/Mixing-Many-Languages_04.jpg"},
+    {number: 5, image: "./img/Mixing-Many-Languages_05.jpg"},
+    {number: 6, image: "./img/Mixing-Many-Languages_06.jpg"}
 ]
 
+InfusingLocalFlavoursP = [
+    {number: 1, image: "./img/Infusing-Local-Flavours_01.jpg"},
+    {number: 2, image: "./img/Infusing-Local-Flavours_02.jpg"},
+    {number: 3, image: "./img/Infusing-Local-Flavours_03.jpg"},
+    {number: 4, image: "./img/Infusing-Local-Flavours_04.jpg"},
+    {number: 5, image: "./img/Infusing-Local-Flavours_05.jpg"},
+    {number: 6, image: "./img/Infusing-Local-Flavours_06.jpg"}
+]
 RenewingTraditionP = [
-    {number: 1, image: "./img/Renewing TraditionP1.png"},
-    {number: 2, image: "./img/Renewing TraditionP2.png"},
-    {number: 3, image: "./img/Renewing TraditionP3.png"},
-    {number: 4, image: "./img/Renewing TraditionP4.png"},
-    {number: 5, image: "./img/Renewing TraditionP5.png"},
-    {number: 6, image: "./img/Renewing TraditionP6.png"}
+    {number: 1, image: "./img/Renewing-Tradition_01.jpg"},
+    {number: 2, image: "./img/Renewing-Tradition_02.jpg"},
+    {number: 3, image: "./img/Renewing-Tradition_03.jpg"},
+    {number: 4, image: "./img/Renewing-Tradition_04.jpg"},
+    {number: 5, image: "./img/Renewing-Tradition_05.jpg"},
+    {number: 6, image: "./img/Renewing-Tradition_06.jpg"}
 ]
 
-window.onload = function(){
+startButton.addEventListener("click", () => {
+    start.classList.add("hide")
+    game.classList.remove("hide")
+    startGame = true
+    Spawn()
+})
+
+playAgain.addEventListener("click", () => {
+    final.classList.add("hide")
+    game.classList.remove("hide")
+    startGame = true
+    Spawn()
+})
+
+function Spawn(){
+    let randomPuzzle = Math.floor(Math.random() * 3);
+    let puzzlePieces
+
+    console.log(randomPuzzle)
+    if(randomPuzzle == 0){
+        puzzlePieces = MixingManyLanguagesP
+    }
+    if(randomPuzzle == 1){
+        puzzlePieces = InfusingLocalFlavoursP
+    }
+    if(randomPuzzle == 2){
+        puzzlePieces = RenewingTraditionP
+    }
+
+    console.log(puzzlePieces)
+
     let i = -1
     //initialize the 3 x 2 board
     for(let r = 1; r < (rows + 1); r ++){
         for(let c = 1; c < (columns + 1); c++){
             let tile = document.createElement("img")
             i = i + 1
-            tile.classList.add(RenewingTraditionG[i].number)
-            tile.src = RenewingTraditionG[i].image
+            tile.classList.add("guide")
+            tile.setAttribute("data", puzzlePieces[i].number)
+            tile.src = puzzlePieces[i].image
 
             //DRAG FUNCTIONALITY
             tile.addEventListener("dragstart", dragStart)   //click on img to drag
@@ -50,7 +114,7 @@ window.onload = function(){
     //pieces
     let pieces = [];
     for(let i = 1; i <= rows*columns; i++){
-        pieces = [...RenewingTraditionP]
+        pieces = [...puzzlePieces]
     }
 
     pieces.reverse();
@@ -66,7 +130,7 @@ window.onload = function(){
     for (let i = 0; i < pieces.length; i ++){
         let tile = document.createElement("img")
         tile.src = pieces[i].image
-        tile.classList.add(pieces[i].number)
+        tile.setAttribute("data", pieces[i].number)
 
         //DRAG FUNCTIONALITY
         tile.addEventListener("dragstart", dragStart)   //click on img to drag
@@ -75,20 +139,25 @@ window.onload = function(){
         tile.addEventListener("dragleave", dragLeave)   //dragging an image away from another one
         tile.addEventListener("drop", dragDrop)         //drop an image onto another
         tile.addEventListener("dragend", dragEnd)       //after youcomplete dragDrop
+        
         document.getElementById("pieces").append(tile)
     }
 }
 
 //DRAG TILES
 function dragStart(){
+    console.log("S")
     currTile = this; //this refers to image that was clicked for dragging
+    console.log(currTile)
 }
 
 function dragOver(e){
+    console.log("o")
     e.preventDefault();
 }
 
 function dragEnter(e){
+    console.log("ent")
     e.preventDefault();
 }
 
@@ -97,27 +166,44 @@ function dragLeave(){
 }
 
 function dragDrop(){
+    console.log("D")
     otherTile = this; //this refers to image that is being dropped on
 }
 
 function dragEnd(){
-    console.log(currTile.classList)
-    console.log(otherTile.classList)
-    if(currTile.src.includes("G") || currTile.src.includes("blank")){
+    if(currTile.classList == null || otherTile.classList == null){
+        return
+    }
+    console.log("c"+ currTile.classList)
+    console.log("o" + otherTile.classList)
+    if(currTile.classList == "guide" || currTile.src.includes("blank")){
         return
     }
 
-    let currValue = currTile.getAttribute("Class")
-    let otherValue = otherTile.getAttribute("Class")
+    let currValue = currTile.getAttribute("data")
+    let otherValue = otherTile.getAttribute("data")
 
+    console.log("c" + currValue)
+    console.log("o", otherValue)
     if(currValue == otherValue){
         otherTile.src = "./img/blank.png"
-        otherTile.classList.remove(otherValue)
+        otherTile.classList.remove("guide")
         otherTile.classList.add("done")
         let currImg = currTile.src
         let otherImg = otherTile.src
         currTile.src = otherImg;
         otherTile.src = currImg;
+
+        let correct = document.querySelectorAll(".done")
+        if(correct.length == 6){
+            let delay = setTimeout(() => {
+                startGame = false
+                remove();
+                final.classList.remove("hide")
+                game.classList.add("hide")
+            }, 1000);
+            
+        }
     }
     else{
         return
@@ -125,15 +211,13 @@ function dragEnd(){
     
 }
 
-function CheckDone(){
-    if(startGame == true){
-        let correct = document.querySelectorAll(".done")
+function remove(){
+    removeAllChildNodes(board)
+    removeAllChildNodes(pieces)
+  }
 
-        console.log(correct.length)
-        if(correct.length == 6){
-            startGame = false
-        }
+  function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
     }
 }
-
-setInterval(CheckDone, 1)
